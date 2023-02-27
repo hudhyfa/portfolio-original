2ac98d5079bb3af1e27cfed27c0677be56b99a6b
hudhyfah <hudhyfahbk@gmail.com>
2023-02-27 17:24:44 +0530

    menu icon issue fixed

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
-
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.menubar');
 
-menu.onclick = () => {
+// menu.onclick = () => {
+//     console.log("click");
+//     menu.classList.toggle ('menu-x');
+//     navbar.classList.toggle ('open');
+// }
+menu.addEventListener('click', () => {
     console.log("click");
-    menu.classList.toggle ('menu-x');
-    navbar.classList.toggle ('open');
-}
+    // menu.classList.toggle ('menu-x');
+    // navbar.classList.toggle ('open');
+    
+})
